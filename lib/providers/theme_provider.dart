9b8bf1d2c6a3e4fa14e2f38387225f8fe5def5c6
AxmedTrooper <shariatullahthesoa@gmail.com>
2025-03-11 02:53:16 +0600

    color

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final isDarkTheme = StateProvider<bool>((ref) => true);
-final backgroundColor = StateProvider<Color>((ref) => Colors.pinkAccent);
+final backgroundColor = StateProvider<Color>((ref) => Colors.blueAccent);
